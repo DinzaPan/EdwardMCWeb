af49d9ee4d2ed7b5a0696cf3d696e47a205b2c0d
Eduard190 <eduardalvarado908@gmail.com>
2025-10-05 08:03:41 -0500

    complement.js

--- js/complement.js
+++ js/complement.js
@@ -14,7 +14,7 @@ const addonsData = [
     {
         id: 2,
         title: "Welcome System",
-        description: "➡️Bienvenido al Addon Welcome System. Dale la bienvenida a tus amigos y jugadores con un sistema. Al unirse a tu mundo, Recibirán Una Bienvenida Perfecto Para Tu Servidor De Minecraft Bedrock.                            ➡️Como Modificar El Script Para Que Sea A Tu Gusto Valla Al Los Archivos Del Addon En Script y Depues En main.js Sino Saves Como En Mi Canal De Youtube Esta El Tuturial YT : Edward Gamer Addon.",
+        description: "➡️Bienvenido al Addon Welcome System. Dale la bienvenida a tus amigos y jugadores con un sistema. Al unirse a tu mundo, Recibirán Una Bienvenida Perfecto Para Tu Servidor De Minecraft Bedrock. ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ➡️Como Modificar El Script Para Que Sea A Tu Gusto Valla Al Los Archivos Del Addon En Script y Depues En main.js Sino Saves Como En Mi Canal De Youtube Esta El Tuturial YT : Edward Gamer Addon.",
         cover_image: "./img/Welcome.jpg",
         version: "1.21.111",
         download_link: "https://cuty.io/slCCvU",
