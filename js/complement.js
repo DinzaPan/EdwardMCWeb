aedf0e9d75970297f649e56cb6d88186d52947e7
Eduard190 <eduardalvarado908@gmail.com>
2025-10-10 08:35:10 -0500

    complement.js

--- js/complement.js
+++ js/complement.js
@@ -2,8 +2,8 @@
 const addonsData = [
     {
         id: 1,
-        title: "$happy-ghast Actions & Stuff 1.6",
-        description: "$1Esta Textura Mejora Tu Mundo Con Animaciones Espectaculares Facil De Activar Para Tu Mundo de Survival O Para Servidores De Minecraft Bedrock",
+        title: "$minecraft Actions & Stuff 1.6",
+        description: "💎Esta Textura Mejora Tu Mundo Con Animaciones Espectaculares Facil De Activar Para Tu Mundo de Survival O Para Servidores De Minecraft Bedrock",
         cover_image: "./img/Action.jpg",
         version: "1.21.113",
         download_link: "https://cuty.io/g37g",
