de7d555261ce6a252074369d58460b66179c3212
Eduard190 <eduardalvarado908@gmail.com>
2025-10-21 15:41:55 -0500

    complement.js

--- js/complement.js
+++ js/complement.js
@@ -4,7 +4,7 @@ const addonsData = [
         id: 1,
         title: "$minecraft Actions & Stuff 1.7",
         description: "💎 !🎃Esta Textura Mejora Tu Mundo Con Animaciones Espectaculares Facil De Activar Para Tu Mundo de Survival O Para Servidores De Minecraft Bedrock",
-        cover_image: "./img/Action.jpg",
+        cover_image: "./img/Action.1.7.jpg",
         version: "1.21.114",
         download_link: "https://cuty.io/obxJIy",
         tags: ["Texturas", "Animación", "Golem Animado"],
